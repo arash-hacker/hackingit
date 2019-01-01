a9f83869c9291581eb5696f3efcf576462efed60
ArashMidos <arshiamidos@gmail.com>
2019-01-01 12:00:00 +0330

    1 on 01 1 2019

--- raw.sh
+++ raw.sh
@@ -25,7 +25,7 @@ do
            #echo $S
            export GIT_COMMITTER_DATE="$Y-$M-$D 12:00:00"
            export GIT_AUTHOR_DATE="$Y-$M-$D 12:00:00"
-           git add . -f 
+           git add commit.md -f 
            git commit --date="$Y-$M-$D 12:00:00" -m "$i on $M $D $Y" 
          done
        else
